2226dcd3eb5f67b44f1dafe6d62b83221f3b1307
Superb-x <943572695@qq.com>
2017-05-11 11:31:49 +0800

    modified the urls patterns

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
-    url(r'^register/', views.register),
-    url(r'^login/', views.login),
+    url(r'^register', views.register),
+    url(r'^login', views.login),
 ]
 
